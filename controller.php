4e48132f70d35ac7a5ff6edc7ef7e83e9f5661dd
christianBogart <christian.biassey-bogart@ashesi.edu.gh>
2015-11-15 19:09:11 +0000

    add nurse function
    this takes first name, last name, age and dob as parameters.

--- controller.php
+++ controller.php
@@ -4,5 +4,20 @@
 
 
 function add_nurse( ){
+	include("users.php");
+	$obj=new users();
+	$firstname=$_REQUEST['firstname'];
+	$lastname=$_REQUEST['lastname'];
+	$age=$_REQUEST['age'];
+    $dob =$_REQUEST['dob'];
+	
+	if(!$obj->add_nurse($firstname,$lastname,$age,$dob)){
+		echo  '{"result":0,"message": "failed to add nurse"}';
+	}
+	else{
+		echo  '{"result":1,"message": "Successfully added nurse"}';
+	
+	}
+}
 	
 ?>
