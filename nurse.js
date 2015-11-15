11c405f94ad9ac7009050a0f626fddd063e992f1
christianBogart <christian.biassey-bogart@ashesi.edu.gh>
2015-11-15 18:24:49 +0000

    Basic add nurse function completed

--- nurse.js
+++ nurse.js
@@ -6,6 +6,15 @@
                 return result;
            }
 
-           
+            function addnurse(){
+				var name = encodeURI(document.getElementById("firstname").value);
+				var user = encodeURI(document.getElementById("lastname").value);
+				var pass = encodeURI(document.getElementById("age").value);
+				var dept = encodeURI(document.getElementById("dob").value);
+				
+				 var url = "controller.php?cmd=1&firstname="+firstname+"&lastname="+lastname+"&age="+age+"&dob="+dob;
+               var obj = sendRequest ( url );
+			  
+			 }
 			 
 			 
