45df655c194bc67c974303644d1bac64a8a0d93e
christianBogart <christian.biassey-bogart@ashesi.edu.gh>
2015-11-15 21:13:21 +0000

    corrected add parameters

--- users.php
+++ users.php
@@ -3,7 +3,7 @@
 class users extends adb
 {	
     
-    function add_nurse($firstname,$lastname,$pass,$dept){
+    function add_nurse($firstname,$lastname,$age,$dob){
        $insert_query= "insert into new_nurse set firstname='$firstname', lastname = '$lastname', age ='$age', dob = '$dob', usertype='nurse'";
         return $this->query ( $insert_query );
     }
